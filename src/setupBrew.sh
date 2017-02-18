b521d0ed3a5c8114347cb4eddf133407c2a09b6f
haxoorx <haxoorx@haxoorx>
2017-02-18 19:37:26 +0100

    primer commit

---
+++ src/setupBrew.sh
@@ -0,0 +1,6 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install ffmpeg
+brew link ffmpeg
+brew tap homebrew/science
+brew install imagemagick
+brew install opencv
---
+++ src/setupPip.sh
@@ -0,0 +1,7 @@
+sudo easy_install pip
+pip install imageio
+pip install opencv-python
+pip install moviepy
+pip install BeautifulSoup
+curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+chmod a+rx /usr/local/bin/youtube-dl
