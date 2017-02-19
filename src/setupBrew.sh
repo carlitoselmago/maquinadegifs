ab0f9fe74550198d0962c5deebe73d3dedb4be18
haxoorx <haxoorx@haxoorx>
2017-02-19 12:47:45 +0100

    primer commit

--- src/setupBrew.sh
+++ src/setupBrew.sh
@@ -3,6 +3,4 @@ brew install python
 brew install ffmpeg --with-vpx --with-vorbis --with-libvorbis --with-vpx --with-vorbis --with-theora --with-libogg --with-libvorbis --with-gpl --with-version3 --with-nonfree --with-postproc --with-libaacplus --with-libass --with-libcelt --with-libfaac --with-libfdk-aac --with-libfreetype --with-libmp3lame --with-libopencore-amrnb --with-libopencore-amrwb --with-libopenjpeg --with-openssl --with-libopus --with-libschroedinger --with-libspeex --with-libtheora --with-libvo-aacenc --with-libvorbis --with-libvpx --with-libx264 --with-libxvid
 brew tap homebrew/science
 brew install imagemagick
-brew install opencv --with-ffmpeg -v
-export PYTHONPATH=/usr/local/lib/python2.7/site-packages:$PYTHONPATH
 export PATH=/usr/local/bin:/usr/local/sbin:~/bin:$PATH
